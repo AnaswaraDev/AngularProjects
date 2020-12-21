7cb64c315d4765d21547f9dd5ed6dcdb239bac17
AnaswaraDev <anaswaradev1998@gmail.com>
2020-12-21 11:30:31 -1100

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,57 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-app1';
-}
+
+  export class AppComponent {
+   title ='my-app1';
+
+   
+   hrefVal='http://google.com';
+   updatedLink = 'oracle.com';
+
+   showMsg =false;
+
+   
+ users = [
+  { userId: 10, firstName: 'jenny'},
+  { userId: 20, firstName: 'jonson'},
+  { userId: 30, firstName: 'robin'},
+  { userId: 56, firstName: 'sherry'},
+  { userId: 50, firstName: 'manson'},
+  { userId: 60, firstName: 'ketty'}
+];
+  
+switchValue= 'three';
+bgColor ='blue';
+colorVal = 'green';
+
+clsName = 'one';
+
+
+
+  }
+
+   
+  
+
+
+
+   /*readfirstname(){
+     console.log(this.firstname);
+   } */
+
+
+
+
+
+
+
+  
+
+   //episode = '17'
+   //subTitle = 'learn interpolation'
+  // firstname ='';
+  
+
+ 
+  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoModule }  from './todo/todo.module';
-  
+import { TasksComponent } from './tasks/tasks.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent 
+    AppComponent,
+    TasksComponent 
   ],
   imports: [
     BrowserModule,
