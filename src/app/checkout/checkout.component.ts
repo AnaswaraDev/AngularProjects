f120eb86228c150f968067006457579f75acc774
AnaswaraDev <anaswaradev1998@gmail.com>
2020-12-27 17:36:19 -1100

    rective form-add-remove -rows

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup ,FormControl, FormBuilder, Validators} from '@angular/forms';
+import { FormGroup ,FormControl, FormBuilder, Validators, FormArray} from '@angular/forms';
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
@@ -18,23 +18,27 @@ export class CheckoutComponent implements OnInit {
       terms: new FormControl() //we can also have it dynamic- will learn it i nxt tutoriak
     });  */
 
+
+ /*  array with 2 form control
+       items :this.formbuilder.array([
+        new FormControl('Angular'),
+        new FormControl('React'),
+
+      ]) */
+
+
+    
+
     this.checkoutForm = formbuilder.group({   //build the form using formbuilder
       emailAddr: [ '' ,[ Validators.minLength(5),
                      Validators.minLength(10), 
                     Validators.required ,
                     Validators.email] ],
       Quantity:  [ '' , Validators.required] ,
-      terms:  [ '' , Validators.requiredTrue] ,
-      items :this.formbuilder.array([
-        new FormControl('Angular'),
-        new FormControl('React'),
+      terms:  [ '' , Validators.requiredTrue] , 
 
-      ])
-
-
-     
-     /* //form array example-1
-      items :this.formbuilder.array([ 
+   
+        items :this.formbuilder.array([ 
         this.formbuilder.group({
           itemId :['1'],
           itemName :['ARC'],
@@ -42,13 +46,38 @@ export class CheckoutComponent implements OnInit {
           itemDone : [ '' ,Validators.requiredTrue  ]
 
         })
-        ]) */
+
+        ]) 
      
-    });
+    }); 
 
   
   }
    ngOnInit(): void { 
+    
+    //getting lemgth
+    //getting idividual array element
+    //set array values for elements
+    //reset array
+    //whatever u can d with array-u can do it with form array
+    
+
+    this.checkoutForm.get('items')?.setValue([{
+      itemId :['1'],
+      itemName :['Anu'],
+      itemDesc :['Rohan ,2'],
+      itemDone : [ '' ,Validators.requiredTrue  ]
+
+      
+    }]);
+
+    //console.log(this.checkoutForm.get('items').value.length);
+     // console.log(this.checkoutForm.get('items').value);
+   //   const itemVal = this.checkoutForm.get('items').value ;
+     // console.log(itemVal[0].itemDesc);
+
+     // this.checkoutForm.get('items').reset();
+
 
      /*this.checkoutForm.get('emailAddr').valueChanges.subscribe(data=> {
         this.valueChangedTracked =data ;
@@ -79,6 +108,28 @@ export class CheckoutComponent implements OnInit {
 
    }
 
+   get items(){
+      return this.checkoutForm.get('items') as FormArray;
+   }
+   // when ever user click on the addnewitem- a new element should be inserted into the form array
+   ///our form array that we are working is items
+   ///form array is an array-we can apply all array methods of know of in any language/js
+   addNewItem(){
+     const itemLength = this.items.length;
+     const newItem = this.formbuilder.group({
+    itemId :[itemLength +1],
+    itemName :[''], 
+    itemDesc :[''],
+    itemDone : [ '' ,Validators.requiredTrue  ]
+
+    
+  });
+     
+this.items.push(newItem);
+   }
+ removeItem(itemId){
+   this.items.removeAt(itemId);
+ }
   
   /* ngOnInit(): void {  
     this.checkoutForm.setValue({   //using set value we are setting entire form value
